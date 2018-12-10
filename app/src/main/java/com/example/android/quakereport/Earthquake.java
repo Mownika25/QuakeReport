03d4a0d988ee4f7d325cd634a8132ae6baeb5eba
Mownika25 <41233359+Mownika25@users.noreply.github.com>
2018-12-10 20:07:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,50 @@
+
+package com.example.android.quakereport;
+/**
+ * {@link Word} represents a vocabulary word that the user wants to learn.
+ * It contains a default translation and a Miwok translation for that word.
+ */
+public class Earthquake {
+    /** Default translation for the word */
+    private double mMagnitude;
+    /** Miwok translation for the word */
+    private String mLocation;
+
+    private String mDate;
+    private long mTimeInMilliseconds;
+    private String mUrl;
+
+    /**
+     * Create a new Word object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation is the word in the Miwok language
+     */
+    public Earthquake(double magnitude, String location, long timeInMilliseconds, String url) {
+        mMagnitude = magnitude;
+        mLocation = location;
+        mTimeInMilliseconds = timeInMilliseconds;
+        mUrl = url;
+    }
+    public long getTimeInMilliseconds() {
+        return mTimeInMilliseconds;
+    }
+
+
+
+    public double getMagnitude() {
+        return mMagnitude;
+    }
+    public String getLocation() {
+        return mLocation;
+    }
+    public String getDate() {
+        return mDate;
+    }
+    public String getUrl() {
+        return mUrl;
+    }
+
+
+}
